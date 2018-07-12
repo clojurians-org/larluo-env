6b456242624e8157ced7acd095e4ea5b0057abbc
clojurians-org <larluo@clojurians.org>
2018-07-12 16:54:46 +0800

    backup emacs file

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,41 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "http")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+
+
+(electric-indent-mode 0)
+
+(require 'cider)
+(require 'paredit)
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+;; (require 'intero)
+;; (intero-global-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (color-theme paredit cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'color-theme)
+(color-theme-initialize)
+(setq color-theme-is-global t)
+;; (color-theme-blue-mood)
+(color-theme-bharadwaj-slate) 
+;; (color-theme-blue-sea)
